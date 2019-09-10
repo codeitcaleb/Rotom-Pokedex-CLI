d69eafa906f61bf19fe65f56b4d4043be7eab08f
Caleb Kavanaugh <calebck@hotmail.com>
2019-09-10 17:08:47 -0400

    Updates to scraper.rb

--- lib/scraper/scraper.rb
+++ lib/scraper/scraper.rb
@@ -1,7 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
  
-html = open("")
+html = open("https://pokemondb.net/pokedex/national")
 pokemon = Nokogiri::HTML(html)
- 
+binding.pry
 pokemon.css("")
+
+# This grabs all 809 Pokemon 
+#pry(main)> pokemon.css(".infocard")
+
+# This grabs the first pokemon- The one at index [0].
+# pokemon.css(".infocard")[0].text
